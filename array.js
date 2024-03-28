cc018e07a81227d5970b5531da40f326ef29aaca
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-27 20:07:16 -0500

    add: MyArray class & methods

--- array.js
+++ array.js
@@ -1,7 +1,25 @@
-const myArray = ['Josué', 'Diego', 'Javier'];
+/* const myArray = ['Josué', 'Diego', 'Javier'];
 
 console.log(myArray.length);
 console.log(myArray);
 
 myArray.push('Cristian');
-console.log(myArray);
+console.log(myArray); */
+
+class MyArray {
+	constructor() {
+		this.length = 0;
+		this.data = {};
+	}
+
+	get(index) {
+		return this.data[index];
+	}
+
+	push(item) {
+		this.data[this.length] = item;
+		this.length++;
+
+		return this.data;
+	}
+}
